a8e1ed6edf6ec71e8405849bf8b969c522896ccb
Philip Levy <philipdlevy@gmail.com>
2022-02-14 08:41:07 -0700

    commit tree 6e31f881c0abfdc4557cf4231a18f48aba2eab19

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
-
-let employee = { 
-    name: "John",
-    streetAddress: "111 Baseline",
+const employee = {
+    name: "philip",
+    streetAddress: "123 West Drive"
 };
 
 function updateEmployeeWithKeyAndValue(employee, key, value) {
     const newEmployee = {...employee, [key]: value};
     return newEmployee;
-};
-
+}
 function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
     employee[key] = value;
     return employee;
-};
+}
 
-function deleteFromEmployeeByKey(employee, ) {
-    const deleteEmployee = {...[employee] };
+function deleteFromEmployeeByKey(employee, key) {
+    const deleteEmployee = {...employee[key]};
     return deleteEmployee;
-};
+}
 
-function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
-    employee[key] = value;
+/*
+function deleteFromEmployeeByKey(employee, key) {
+    const copyEmployee = {...employee};  
+    delete copyEmployee[key]
+    return copyEmployee;
+    }
+*/
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
     return employee;
-};
-
-
-
-
+}
 
 
 
